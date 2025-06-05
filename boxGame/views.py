e185455465ca1a6122a952f3b81633e0cfe5168c
max vlad <vladykinmv@gmail.com>
2025-06-05 22:01:47 +0300

    UTF-8 json response

--- boxGame/views.py
+++ boxGame/views.py
@@ -123,4 +123,4 @@ def getAllGamers(request):
             'Position': gamer.position
         })
 
-    return JsonResponse(resultArray, safe=False)
+    return JsonResponse(resultArray, safe=False, json_dumps_params={'ensure_ascii': False})
